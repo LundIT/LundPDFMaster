75615d577a53e1fe9528684b8d431c7b36c1447e
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2023-06-23 16:35:51 +0300

    update packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="LundPDFMaster",
     version="0.1",
-    packages=find_packages(),
+    packages=["lund_pdf_master"],
     url="https://github.com/LundIT/LundPDFMaster",
     license="MIT",
     author="Ensar Kaya",
